35ec0e31028f250d6d8855aec744925385fbfd29
alexandru.mireuta <alexandru.mireuta@stsnet.ro>
2025-01-29 11:14:56 +0200

    Filtru coloane componenta

--- src/pages/cheltuieli/lista/components/TabelCheltuieli.tsx
+++ src/pages/cheltuieli/lista/components/TabelCheltuieli.tsx
@@ -25,6 +25,7 @@ import { Button } from '@/components/ui/button.tsx';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input.tsx';
 import { PagingFooterTabel } from '@/components/ui/PagingFooterTabel.tsx';
+import { FiltruColoaneCheltuieli } from '@/pages/cheltuieli/lista/components/FiltruColoaneCheltuieli.tsx';
 
 export type FiltreCheltuialaType = {
   an: number;
@@ -118,54 +119,7 @@ export const TabelCheltuieli: React.FC = () => {
           filtre={filtre}
           setFiltre={setFiltre}
         />
-        <Popover onOpenChange={(open) => (open ? table.resetColumnFilters() : null)}>
-          <PopoverTrigger asChild>
-            <Button variant="outline">Filtru</Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-80">
-            <div className="grid gap-4">
-              <div className="space-y-2">
-                <h4 className="font-medium leading-none">Filtru cheltuieli</h4>
-              </div>
-              <div className="grid grid-cols-3 items-center gap-4">
-                <Label htmlFor="descriere">Descriere</Label>
-                <Input
-                  id="descriere"
-                  className="col-span-2 h-8"
-                  type="text"
-                  onChange={(value) => {
-                    table?.getColumn('descriere')?.setFilterValue(value.target.value);
-                  }}
-                  placeholder={`Search...`}
-                />
-              </div>
-              <div className="grid grid-cols-3 items-center gap-4">
-                <Label htmlFor="descriere">Suma de la</Label>
-                <Input
-                  id="suma"
-                  className="col-span-2 h-8"
-                  type="number"
-                  onChange={(value) =>
-                    table?.getColumn('suma')?.setFilterValue((old: [number, number]) => [value.target.value, old?.[1]])
-                  }
-                  placeholder={`Min`}
-                />
-              </div>
-              <div className="grid grid-cols-3 items-center gap-4">
-                <Label htmlFor="descriere">Suma pana la</Label>
-                <Input
-                  id="suma"
-                  className="col-span-2 h-8"
-                  type="number"
-                  onChange={(value) =>
-                    table?.getColumn('suma')?.setFilterValue((old: [number, number]) => [old?.[0], value.target.value])
-                  }
-                  placeholder={`Max...`}
-                />
-              </div>
-            </div>
-          </PopoverContent>
-        </Popover>
+        <FiltruColoaneCheltuieli table={table} />
         <SumarCheltuieli filtre={filtre} />
       </div>
       <Table className="w-full table-auto">
