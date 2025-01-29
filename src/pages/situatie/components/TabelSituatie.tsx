7953c6166456537e68a4b18a9ecf2723d50f70a6
Rosu Cosmin <cosmin.rosu@stsnet.ro>
2025-01-29 11:47:12 +0200

    tabel situatie cu react table

--- src/pages/situatie/components/TabelSituatie.tsx
+++ src/pages/situatie/components/TabelSituatie.tsx
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table.tsx';
 import { useCalculeazaSituatie } from '@/pages/situatie/hooks/useCalculeazaSituatie.tsx';
-import { twMerge } from 'tailwind-merge';
-import { Button } from '@/components/ui/button.tsx';
-import { NavLink } from 'react-router-dom';
-import { FaInfo } from 'react-icons/fa';
+import { FaArrowDown, FaArrowUp } from 'react-icons/fa';
 import { FiltreSituatie } from '@/pages/situatie/components/FiltreSituatie.tsx';
 import { ApaSauCafea } from '@/fake-api/fakePaymentApi.ts';
+import { SumarCheltuieli } from '@/pages/cheltuieli/lista/components/SumarCheltuieli.tsx';
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+import { PagingFooterTabel } from '@/components/ui/PagingFooterTabel.tsx';
+import { twMerge } from 'tailwind-merge';
 import { Badge } from '@/components/ui/badge.tsx';
+import { FiltruColoaneSituatie } from '@/pages/situatie/components/FiltruColoaneSituatie.tsx';
 
 export const LunileAnului = {
   IANUARIE: 'Ianuarie',
@@ -39,28 +49,76 @@ export type FiltreSituatieType = {
   pentru: ApaSauCafea;
 };
 
+const getMonthCellColor = (value: number, luna: Luna, an: number) => {
+  const lunaCurenta = new Date().getMonth();
+  const anCurent = new Date().getFullYear();
+
+  const lunaIndex = Object.keys(LunileAnului).findIndex((key) => key === luna);
+
+  if (lunaIndex > lunaCurenta && anCurent <= an) {
+    if (value > 0) {
+      return 'text-purple-500';
+    }
+    return 'text-primary';
+  }
+
+  if (value === 40) {
+    return 'text-green-700';
+  }
+
+  if (value > 0) {
+    return 'text-yellow-700';
+  }
+
+  return 'text-red-700';
+};
+
 export const TabelSituatie: React.FC = () => {
   const [filtre, setFiltre] = useState<FiltreSituatieType>({ an: 2025, pentru: 'cafea' });
   const situatii = useCalculeazaSituatie(filtre);
 
-  const getMonthCellColor = (value: number, luna: Luna, an: number) => {
-    const lunaCurenta = new Date().getMonth();
-    const anCurent = new Date().getFullYear();
+  const luniColumnDefs: ColumnDef<SituatiePersoana>[] = Object.entries(LunileAnului).map(([key, value], index) => {
+    return {
+      accessorKey: key,
+      header: value,
+      accessorFn: (originalRow) => originalRow.luni[key as Luna],
+      cell: (info) => (
+        <div
+          className={twMerge(
+            'text-lg font-bold',
+            getMonthCellColor(info.row.original.luni[key as Luna], key as Luna, filtre.an)
+          )}>
+          {info.row.original.luni[key as Luna]}
+        </div>
+      ),
+    };
+  });
 
-    const lunaIndex = Object.keys(LunileAnului).findIndex((key) => key === luna);
+  const columns: ColumnDef<SituatiePersoana>[] = [
+    {
+      id: 'fullName',
+      header: 'Nume',
+      accessorFn: (row) => `${row.nume} ${row.prenume}`,
+    },
+    {
+      id: 'laZi',
+      header: 'Status',
+      accessorKey: 'laZi',
+      cell: (info) =>
+        info.getValue() ? <Badge variant={'success'}>La zi</Badge> : <Badge variant={'destructive'}>Restantier</Badge>,
+    },
+    ...luniColumnDefs,
+  ];
 
-    if (lunaIndex > lunaCurenta && anCurent <= an) return 'text-primary';
-
-    if (value === 40) {
-      return 'text-green-700';
-    }
-
-    if (value > 0) {
-      return 'text-yellow-700';
-    }
-
-    return 'text-red-700';
-  };
+  const table = useReactTable({
+    columns,
+    data: situatii,
+    // onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(), //client side filtering
+    getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  });
 
   // TODO:
   // if (isLoading || !situatii) {
@@ -74,71 +132,73 @@ export const TabelSituatie: React.FC = () => {
   // }
 
   return (
-    <>
-      <FiltreSituatie
-        filtre={filtre}
-        setFiltre={setFiltre}
-      />
-      <Table>
+    <div className={'flex flex-col gap-3'}>
+      <div className="flex items-center justify-between">
+        <FiltreSituatie
+          filtre={filtre}
+          setFiltre={setFiltre}
+        />
+        <FiltruColoaneSituatie table={table} />
+        <SumarCheltuieli filtre={filtre} />
+      </div>
+      <Table className="w-full table-auto">
         <TableHeader>
-          <TableRow>
-            <TableHead>Index</TableHead>
-            <TableHead>Nume</TableHead>
-
-            {Object.keys(LunileAnului).map((key) => (
-              <TableHead key={key}>{LunileAnului[key as Luna]}</TableHead>
-            ))}
-
-            <TableHead>Actiuni</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <tr>
-            <td
-              className={'h-2.5 border-b'}
-              colSpan={7}>
-              {/*{isFetching && <ProgressBar mode={'indeterminate'} />}*/}
-            </td>
-          </tr>
-
-          {situatii.map((situatie, index) => (
-            <TableRow
-              key={situatie.userId}
-              className={'odd:bg-slate-50 hover:bg-slate-100'}>
-              <TableCell className="font-medium">{index + 1}</TableCell>
-              <TableCell className="font-medium">
-                <div className={'flex flex-row justify-between'}>
-                  {`${situatie.nume} ${situatie.prenume}`}
-
-                  {situatie.laZi ? (
-                    <Badge variant={'success'}>La zi</Badge>
-                  ) : (
-                    <Badge variant={'destructive'}>Restantier</Badge>
-                  )}
-                </div>
-              </TableCell>
-              {Object.keys(LunileAnului).map((key) => (
-                <TableCell
-                  key={key}
-                  className={twMerge(
-                    'text-lg font-bold',
-                    getMonthCellColor(situatie.luni[key as Luna], key as Luna, filtre.an)
-                  )}>
-                  {situatie.luni[key as Luna]}
-                </TableCell>
-              ))}
-
-              <TableCell className={'max-w-[200px]'}>
-                <Button asChild={true}>
-                  <NavLink to={`/persoana/${situatie.userId}`}>
-                    <FaInfo /> Detalii
-                  </NavLink>
-                </Button>
-              </TableCell>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead
+                    key={header.id}
+                    colSpan={header.colSpan}>
+                    {header.isPlaceholder ? null : (
+                      <div
+                        className={
+                          header.column.getCanSort()
+                            ? 'flex cursor-pointer select-none content-center items-center gap-1'
+                            : 'flex content-center items-center'
+                        }
+                        onClick={header.column.getToggleSortingHandler()}>
+                        {flexRender(header.column.columnDef.header, header.getContext())}
+                        {{
+                          asc: <FaArrowUp />,
+                          desc: <FaArrowDown />,
+                        }[header.column.getIsSorted() as string] ?? null}
+                      </div>
+                    )}
+                  </TableHead>
+                );
+              })}
             </TableRow>
           ))}
+        </TableHeader>
+        <TableBody>
+          {/*TODO:*/}
+          {/*<tr>*/}
+          {/*  <td*/}
+          {/*    className={'h-2.5 border-b'}*/}
+          {/*    colSpan={7}>*/}
+          {/*    {isFetching && <ProgressBar mode={'indeterminate'} />}*/}
+          {/*  </td>*/}
+          {/*</tr>*/}
+
+          {table.getRowModel().rows.map((row) => {
+            return (
+              <TableRow key={row.id}>
+                {row.getVisibleCells().map((cell) => {
+                  return (
+                    <TableCell
+                      className="font-medium"
+                      key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
+            );
+          })}
         </TableBody>
       </Table>
-    </>
+      <PagingFooterTabel table={table} />
+    </div>
   );
 };
