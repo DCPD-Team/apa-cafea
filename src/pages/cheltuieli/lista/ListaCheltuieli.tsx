12e7de6f01dc6b3627d1da8ecc56a186159fac4a
Rosu Cosmin <cosmin.rosu@stsnet.ro>
2025-02-20 11:21:50 +0200

    fix styling

--- src/pages/cheltuieli/lista/ListaCheltuieli.tsx
+++ src/pages/cheltuieli/lista/ListaCheltuieli.tsx
@@ -122,6 +122,8 @@ export const ListaCheltuieli: React.FC = () => {
             table={table}
             isFetching={isFetching}
             isLoading={isLoading || !cheltuieli}
+            cols={7}
+            rows={15}
           />
         </div>
       </CardContent>
