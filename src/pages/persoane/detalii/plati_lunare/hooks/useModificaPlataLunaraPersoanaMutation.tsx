cb19804b3cb191797ea1b7f63aeee1a5956ce851
victorita.boerescu <vikiboerescu@Vikis-MacBook-Pro.local>
2025-03-04 16:39:17 +0200

    plati lunare small fixes

--- src/pages/persoane/detalii/plati_lunare/hooks/useModificaPlataLunaraPersoanaMutation.tsx
+++ src/pages/persoane/detalii/plati_lunare/hooks/useModificaPlataLunaraPersoanaMutation.tsx
@@ -56,6 +56,9 @@ export const useModificaPlataLunaraPersoanaMutation = ({
       queryClient.invalidateQueries({
         queryKey: ['balance', personId],
       });
+      queryClient.invalidateQueries({
+        queryKey: ['expenses', plataLunara.target_year, plataLunara.expense_type_id],
+      });
 
       close();
       toast({
